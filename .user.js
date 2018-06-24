7b47279d39de7673710efffbbc53bf72a90254ea
Zeker0 <zorn@hotmail.co.uk>
2018-06-24 16:06:18 +0100

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
-// @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
+// @name         AutoTrimps-Zekbeta
+// @version      1.0-Zekbeta
+// @namespace    https://Zorn192.github.io/AT
+// @updateURL    https://Zorn192.github.io/AT/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *Zorn192.github.io/AutoTrimps*
+// @connect      *Zorn192.github.io/AT*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-Zekbeta';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://Zorn192.github.io/AT/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
